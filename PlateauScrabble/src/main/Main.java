5935b229b54bc86f55ae8f66ec55b2aa127ba28d
DontBeAJerry <31726023+DontBeAJerry@users.noreply.github.com>
2017-10-07 19:02:35 +0200

    Add files via upload
    Esquisse plateau Scene Builder

---
+++ PlateauScrabble/src/main/Main.java
@@ -0,0 +1,63 @@
+package main;
+
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+
+public class Main {
+
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Plateau de Scrabble");
+
+		initRootLayout();
+
+	}
+
+	/**
+	 * Returns the main stage.
+	 * 
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+	/**
+	 * Initializes the root layout.
+	 */
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
